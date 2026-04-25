c8dafa1d0c1d42a6b7ad998425ccc5b0090522b2
Yusuf-Ayba <bayuadiyatma317@gmail.com>
2026-04-25 15:06:27 +0700

    upload project pertama

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const burger = document.getElementById("burger");
+const navMenu = document.getElementById("nav-menu");
+
+burger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
+
